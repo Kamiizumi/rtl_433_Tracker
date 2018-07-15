a9c865c331844400e5ef26ea1c395094851d27e5
Ben Newman <BenNewman@outlook.com>
2018-07-15 17:28:33 +0100

    Migrate DB on startup

--- Rtl433Tracker.Web/Program.cs
+++ Rtl433Tracker.Web/Program.cs
@@ -5,8 +5,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Rtl433Tracker.Data;
 
 namespace Rtl433Tracker.Web
 {
@@ -17,9 +20,20 @@ namespace Rtl433Tracker.Web
             BuildWebHost(args).Run();
         }
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            var webHost = WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
                 .Build();
+
+            using (var scope = webHost.Services.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetService<Rtl433TrackerContext>();
+                db.Database.Migrate();
+            }
+
+            return webHost;
+        }
+            
     }
 }
