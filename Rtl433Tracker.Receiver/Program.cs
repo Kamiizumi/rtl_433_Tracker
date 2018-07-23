6f3279f7e5ae2a7ec7f5ae50cbda80fa87e8c6b4
Ben Newman <BenNewman@outlook.com>
2018-07-23 21:39:01 +0100

    Re-order usings

--- Rtl433Tracker.Receiver/Program.cs
+++ Rtl433Tracker.Receiver/Program.cs
@@ -1,10 +1,10 @@
-﻿using Microsoft.Extensions.Configuration;
-using System;
+﻿using System;
 using System.Diagnostics;
 using System.IO;
 using System.Net.Http;
 using System.Text;
 using System.Threading;
+using Microsoft.Extensions.Configuration;
 
 namespace Rtl433Tracker.Receiver
 {
