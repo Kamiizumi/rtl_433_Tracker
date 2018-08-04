fc5a57a507ed9d445893daf6dfefe704940c4c56
Ben Newman <BenNewman@outlook.com>
2018-08-05 00:20:21 +0100

    Add endpoint to get latest events

---
+++ Rtl433Tracker.Api.Models/DataTransferObjects/Events/EventDetails.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Rtl433Tracker.Api.Models.DataTransferObjects.Events
+{
+    public class EventDetails
+    {
+        public Guid Id { get; set; }
+
+        public DateTime Time { get; set; }
+
+        public Guid DeviceId { get; set; }
+
+        public string DriverModel { get; set; }
+
+        public string DriverId { get; set; }
+    }
+}
