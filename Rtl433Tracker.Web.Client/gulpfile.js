b76e02f9be012ef389f83a7d17ead495b010322c
Ben Newman <BenNewman@outlook.com>
2018-08-04 21:38:02 +0100

    Install bootstrap via npm / gulp

---
+++ Rtl433Tracker.Web.Client/gulpfile.js
@@ -0,0 +1,11 @@
+﻿var gulp = require('gulp');
+var del = require('del');
+
+gulp.task('clean', function () {
+    return del(['wwwroot/libs/**']);
+})
+
+gulp.task('css', function () {
+    return gulp.src('node_modules/bootstrap/dist/css/bootstrap.min.css')
+        .pipe(gulp.dest('wwwroot/libs/bootstrap/dist/css'));
+});
